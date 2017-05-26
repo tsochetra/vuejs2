e8ed389cb4e7eaf26b39c9c2a1093c630fe128d6
tsochetra <than.sochetra@icloud.com>
2017-05-26 15:30:07 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,47 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+   <div>
+      <input type="text" v-model="search">
+      <ul>
+         <li v-for="(data,index) in searchFil">
+            {{ data.userId }} - {{ data.title}}
+         </li>
+      </ul>
+   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+   name: 'app',
+   components: {
+
+   },
+   data () {
+      return {
+         msg: 'Welcome to Your Vue.js App',
+         datas: [],
+         search: ""
+      }
+   },
+   created() {
+      this.$http.get("http://jsonplaceholder.typicode.com/posts").then((data) => {
+         this.datas = data.body
+         console.log(data.body)
+      }).catch((err)=>{
+         console.log("err")
+      })
+
+   },
+   computed: {
+      searchFil: function() {
+         return this.datas.filter((math) => {
+            return math.title.match(this.search)
+         })
+      }
+   }
+
+   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
